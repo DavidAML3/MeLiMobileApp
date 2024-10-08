4e818b3cf91a65b3cd12e80c00b8c1dd4a122412
DavidAML3 <davidaml@Davids-MacBook-Pro.local>
2024-10-07 22:33:00 -0500

    Work in log error for APIServices and refactor

--- MeLiMobileApp/Services/APIService.swift
+++ MeLiMobileApp/Services/APIService.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import Combine
+import os.log
 
 enum APIError: Error {
     case invalidResponse
-    case networkError
+    case networkError(Error)
+    case decodingError(Error)
 }
 
 protocol APIServiceProtocol {
@@ -20,37 +22,55 @@ protocol APIServiceProtocol {
 }
 
 class APIService: APIServiceProtocol {
-    func fetchProducts(url: String) -> AnyPublisher<[Product], Error> {
+    private let logger = Logger(subsystem: "com.MeLiMobileApp", category: "network")
+    
+    private func makeRequest<T: Decodable>(url: String, decodeTo type: T.Type) -> AnyPublisher<T, Error> {
         guard let url = URL(string: url) else {
+            logger.error("Invalid URL: \(url)")
             return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
         }
         
         return URLSession.shared.dataTaskPublisher(for: url)
-            .map(\.data)
-            .decode(type: SearchResult.self, decoder: JSONDecoder())
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                    self.logger.error("Invalid HTTP response: \(response)")
+                    throw APIError.invalidResponse
+                }
+                return data
+            }
+            .decode(type: type, decoder: JSONDecoder())
+            .mapError { error in
+                self.logError("makeRequest", error: error)
+                return self.mapApiError(error)
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func fetchProducts(url: String) -> AnyPublisher<[Product], Error> {
+        makeRequest(url: url, decodeTo: SearchResult.self)
             .map { $0.results }
             .eraseToAnyPublisher()
     }
 
     func fetchProductDetail(url: String) -> AnyPublisher<Product, Error> {
-        guard let url = URL(string: url) else {
-            return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
-        }
-
-        return URLSession.shared.dataTaskPublisher(for: url)
-            .map(\.data)
-            .decode(type: Product.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
+        makeRequest(url: url, decodeTo: Product.self)
     }
 
     func fetchProductDescription(url: String) -> AnyPublisher<Description, Error> {
-        guard let url = URL(string: url) else {
-            return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+        makeRequest(url: url, decodeTo: Description.self)
+    }
+    
+    private func logError(_ functionName: String, error: Error) {
+        logger.error("Error in \(functionName): \(error.localizedDescription)")
+    }
+    
+    private func mapApiError(_ error: Error) -> APIError {
+        if let urlError = error as? URLError {
+            return .networkError(urlError)
+        } else if let decodingError = error as? DecodingError {
+            return .decodingError(decodingError)
+        } else {
+            return .invalidResponse
         }
-
-        return URLSession.shared.dataTaskPublisher(for: url)
-            .map(\.data)
-            .decode(type: Description.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
     }
 }
